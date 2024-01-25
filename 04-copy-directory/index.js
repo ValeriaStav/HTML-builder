71848275a7a2535d6ad2caa7c461d84b62dbfd24
ValeriaStav <valeriiastavriadi@gmail.com>
2024-01-25 18:02:54 +0300

    feat: execute 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const myPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  await fs.promises.rm(newPath, { recursive: true });
+  await fs.promises.mkdir(newPath, { recursive: true });
+  (await fs.promises.readdir(myPath, { withFileTypes: true })).forEach(
+    (file) => {
+      fs.copyFile(
+        path.join(myPath, file.name),
+        path.join(newPath, file.name),
+        (err) => {
+          if (err) throw err;
+        },
+      );
+    },
+  );
+}
+
+copyDir();
